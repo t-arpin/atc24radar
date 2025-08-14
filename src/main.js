87fed9d94eca1970126de85d22370b21a962a884
t-arpin <patches9090@gmail.com>
2025-08-13 23:09:10 -0400

    renamed public to docs for github pages

--- src/main.js
+++ src/main.js
@@ -42,7 +42,7 @@ const inFlightSVG = `<svg  id="plane-icon" xmlns="http://www.w3.org/2000/svg"  w
 const onGroundSVG = `<svg  id="plane-icon" xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-plane-inflight"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M15 11.085h5a2 2 0 1 1 0 4h-15l-3 -6h3l2 2h3l-2 -7h3l4 7z" /><path d="M3 21h18" /></svg>`;
 
 //websocket localhost port
-const PORT = 4000;
+const PORT = 8080;
 
 let zuluTime = false;
 let isDraggingLabel = { bool: false, label: null };
